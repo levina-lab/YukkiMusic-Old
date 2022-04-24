a88c09f9c48b90c67989bde019f65ec4f292346b
Panda <100751249+mcdygaming@users.noreply.github.com>
2022-04-24 21:56:18 +0700

    Update music.py

--- Yukki/Plugins/music.py
+++ Yukki/Plugins/music.py
@@ -64,10 +64,7 @@ def convert_seconds(seconds):
 @Client.on_message(command(["play", "play@VeezMegaBot"]) & other_filters)
 async def play(_, message: Message):
     await message.delete()
-    chat_id = message.chat.id
-    if not await is_served_chat(chat_id):
-        await message.reply(f"❌ **This chat not authorized !**\n\nI can't stream music in non-authorized chat, ask to sudo user to auth this chat.\n\nCheck the sudo user list [From Here](https://t.me/{BOT_USERNAME}?start=sudolist)", disable_web_page_preview=True)
-        return await app.leave_chat(chat_id)  
+    chat_id = message.chat.id  
     if message.sender_chat:
         return await message.reply_text("You'll need to switch to a user account to play music.")  
     user_id = message.from_user.id
